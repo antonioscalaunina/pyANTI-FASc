d2df02763280c4b350cc154a26ac57ba1f319120
Antonio Scala <90616800+antonioscalaunina@users.noreply.github.com>
2026-04-13 16:11:49 +0200

    Add files via upload
    Simplified antifasc_main.py file. Improved running time

--- bin/antifasc_main.py
+++ bin/antifasc_main.py
@@ -24,15 +24,14 @@ import cartopy.feature as cfeature
 
 
 #Specify input json file and scaling_relationship file paths
-input_file='../config_files/Parameters/input.json'
-scaling_file='../config_files/Parameters/scaling_relationship.json'
+input_file='../config_files/Parameters/input_Samos.json'
+scaling_file='../config_files/Parameters/scaling_relationship_WC.json'
 
 #Initialize an instance of the class Slab with the input files
 Slab_obj=slab.Slab(input_file,scaling_file)
 
 
-# In[3]:
-
+# In[3]:  THIS PART IS ONLY FOR PLOTTING IN THE JN - COMMENTED IN THE .PY WORKFLOW
 
 #plot slab and SPDF (Slip Probability Density Function)
 #print('PLOT SLAB MESH AND THE COMPUTED SPDF')
@@ -67,12 +66,6 @@ Slab_obj=slab.Slab(input_file,scaling_file)
 #gl.right_labels = False
 
 
-
-# 
-# 
-# 
-# 
-# 
 # ### 2. SELECTION OF RUPTURE BARYCENTERS
 # 
 # The second step will generate a selection of rupture barycenters having a fixed `minimum_interdistance`
@@ -81,11 +74,18 @@ Slab_obj=slab.Slab(input_file,scaling_file)
 # For this, apply the method `active_barycenters()` to and subsequently the method `select_barycenter2()`.
 # 
 # 
-# 
+#
+
+# ### 3. RUPTURE AREAS COMPUTE
+
+# First we need to compute or load the matrix distance and the connectivity matrix of the mesh, and compute the mesh' elements areas
 
 # In[4]:
+Slab_obj.compute_matrix_distance()
+Slab_obj.compute_area()
+Slab_obj.Element2Element()
 
-
+# In[5]:
 #Select active barycenters for the whole subduction 
 Slab_obj.active_barycenters()
 #Selection of barycenters for case-study or hazard
@@ -93,109 +93,70 @@ Slab_obj.select_barycenter2()
 
 
 # We can check the Mw and scaling relationships for which rupturing barycenters were selected
-
-# In[5]:
-
-
+# In[6]:
 print(f'Mw: {Slab_obj.get_magnitudes()}')
 print(f'Scaling names: {Slab_obj.Name_scaling}')
 
 
 # Now, let's choose from the lists above a combination of magnitude and scaling relationship and plot the respecive rupturing barycenters. 
 
-# In[6]:
-
-
+# In[7]: THIS PART IS ONLY FOR PLOTTING IN THE JN - COMMENTED IN THE .PY WORKFLOW
 #plot barycenters for the specified combination of Mw and scaling relationship
 #Mw=8.8846
 #scaling='Murotani'
-
 #print(f'PLOT BARYCENTERS FOR  Mw={Mw} and SCALING NAME: {scaling}')
-
 #fig,ax = plt.subplots(1,1,figsize=(6, 6),subplot_kw={'projection': ccrs.PlateCarree()})
-
 # Create a GeoAxes in the tile's projection
-
 # Add coastlines and features
 #ax.coastlines(resolution='10m',linewidth=0.7)
 #ax.add_feature(cfeature.LAND, linestyle=':',color='gainsboro')
 #ax.add_feature(cfeature.OCEAN, linestyle=':',color='#0085BD')
-
 # Add gridlines
 #gl = ax.gridlines(draw_labels=True, alpha=0)
 #gl.top_labels = False
 #gl.right_labels = False
-
 #plot Slab mesh
 #Slab_obj.plot_slab(ax,colorbar=True)
-
 #PLOT BARYCENTERS
 #Slab_obj.plot_barycenters_mag(Mw,scaling,ax)
-
-
-# 
-# ### 3. RUPTURE AREAS COMPUTE
-
-# First we need to compute or load the matrix distance and the connectivity matrix of the mesh, and compute the mesh' elements areas
-
-# In[ ]:
-
-Slab_obj.compute_matrix_distance()
-Slab_obj.compute_area()
-Slab_obj.Element2Element()
 #Slab_obj.compute_matrix_distance()
 
 
 # Compute the rupturing areas for every barycenter in each combination of magnitude and scaling relationship by applying the method `rupture_areas()`
 
-# In[ ]:
-
-
+# In[8]:
 Slab_obj.rupture_areas()
 
 
 # Likewise, we can check the number of rupture areas computed for each combination of magnitude and scaling relationship name
 
-# In[ ]:
-
-
+# In[9]:
 Slab_obj.get_RuptAreas_number()
 
 
 # Now, let's plot an example of a computed rupturing area for the scenario $M_w$=8.8846 and scaling relationship: *Murotani*
 
-# In[ ]:
-
-
+# In[10]: THIS PART IS ONLY FOR PLOTTING IN THE JN - COMMENTED IN THE .PY WORKFLOW
 #plot barycenters for the specified combination of Mw and scaling name
 #Mw=8.8846
 #scaling='Murotani'
 #Rupture_area_id=5
-
 #print(f'PLOT BARYCENTERS FOR  Mw={Mw} and SCALING NAME: {scaling}')
-
 #fig,ax = plt.subplots(1,1,figsize=(6, 6),subplot_kw={'projection': ccrs.PlateCarree()})
-
 # Create a GeoAxes in the tile's projection
-
 # Add coastlines and features
 #ax.coastlines(resolution='10m',linewidth=0.7)
 #ax.add_feature(cfeature.LAND, linestyle=':',color='gainsboro')
 #ax.add_feature(cfeature.OCEAN, linestyle=':',color='#0085BD')
-
 # Add gridlines
 #gl = ax.gridlines(draw_labels=True, alpha=0)
 #gl.top_labels = False
 #gl.right_labels = False
-
 #Slab_obj.plot_rupture_area(Mw,scaling,Rupture_area_id,ax,True)
 
 
 # Finally, write the output files
-
-# In[ ]:
-
-
+# In[11]:
 #Write Output
 Slab_obj.write_output_rupture_areas()
 #generate folder tree for outputs
@@ -203,19 +164,17 @@ Slab_obj.generate_foldertree_slip()
 
 
 # ### SLIP DISTRIBUTION COMPUTATION ###
-
-# In[ ]:
-
-
+# In[12]:
 #compute slipe distributions
 Slab_obj.slip_distribution()
 
 
 # In[13]:
-
-
 end_time = time.time()
-print( end_time - start_time)
+elapsed = end_time - start_time
+minutes = int(elapsed // 60)
+seconds = elapsed % 60
+print(f"Total running time: {minutes} min {seconds:.2f} sec")
 
 
 # In[ ]:
